2042616252f3eb54b5a057a9ae5f993811bf7c76
Steven Raden <sraden@uw.edu>
2015-02-08 17:11:07 -0800

    Dragging works, but clicking on a table edge breaks it

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 "use strict";
 (function() {
 
-	$(document).ready(function() {
-		
-	});
+    var mouseDown = false;
 
+    $(document).ready(function() {
+        $('td').hover(function(e) {
+            //console.log(mouseDown);
+            pauseEvent(e);
+            if (mouseDown) {
+                //if (!$(this).hasClass('.tableActive')) {
+                    $(this).addClass('tableActive');
+                //}
+            }
+        });
+
+        $(document).mousedown(function() { 
+            mouseDown = true;
+        });
+        $(document).mouseup(function() {
+            mouseDown = false;
+        });
+
+    });
+
+    function pauseEvent(e){
+        e.stopPropagation();
+        e.preventDefault();
+        return false;
+    }
 
 })();
