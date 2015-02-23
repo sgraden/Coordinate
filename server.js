8464c8db1fd5510c839b6e07a73f44d3af37a39f
Steven Raden <stevengraden@gmail.com>
2015-02-23 14:41:01 -0800

    BLAH

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 //console.log(process.argv);
 
-var arg = process.argv
-var sum = 0;
-for (var i = 2; i < arg.length; i++) {
-	sum += +arg[i];
-}
+var express = require('express');
+var app = express();
+var cool = require('cool-ascii-faces');
 
-console.log(sum);
+
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
+
+// app.get('/', function(request, response) {
+//   response.send(cool());
+// });
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
