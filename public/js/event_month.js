275a4f18e3fe444e21c456be8523c50eacbe70ba
Steven Raden <stevengraden@gmail.com>
2015-04-14 16:10:14 -0700

    month calendar visible

---
+++ public/js/event_month.js
@@ -0,0 +1,30 @@
+"use strict";
+(function() {
+
+$(document).ready(function() {
+	createMonthCal();
+});
+
+function createMonthCal() {
+	$('#month-calendar').fullCalendar({
+		height: 600,
+		selectable: true,
+		selectHelper: true,
+		select: function(start, end) {
+			var check = $.fullCalendar.formatDate(start,'yyyy-MM-dd');
+		    var today = $.fullCalendar.formatDate(new Date(),'yyyy-MM-dd');
+		    if(check < today)
+		    {
+		        return null;
+		    }
+		    else
+		    {
+		        // Its a right date
+		                // Do something
+		    }
+		},
+		fixedWeekCount: false
+	});
+}
+
+})();
