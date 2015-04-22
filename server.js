122fd36f8e6543c31233893d161186d4e1151b03
Steven Raden <stevengraden@gmail.com>
2015-04-22 01:44:28 -0700

    error posting

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var pg = require('pg');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '/public/'));
+app.use(express.static(__dirname + '/public/home.html'));
 
 //app.get('/', function(request, response) {
 //	response.send(cool());
