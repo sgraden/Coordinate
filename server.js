eadb1fdbdded92d1dcafd4d631d6849a3baea802
Steven Raden <stevengraden@gmail.com>
2015-04-28 00:18:54 -0700

    server disconnect handler?

--- server.js
+++ server.js
@@ -78,19 +78,19 @@ var db_config = {
 	user     : 'b18e443b2960cf',
 	password : '1a13ae39'
 }
-var conn;// = mysql.createConnection(db_config);
+var conn = mysql.createConnection(db_config);
 
 function handleDisconnect() {
-  connection = mysql.createConnection(db_config); // Recreate the connection, since
+  conn = mysql.createConnection(db_config); // Recreate the connection, since
                                                   // the old one cannot be reused.
-  connection.connect(function(err) {              // The server is either down
+  conn.connect(function(err) {              // The server is either down
     if(err) {                                     // or restarting (takes a while sometimes).
       console.log('error when connecting to db:', err);
       setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
     }                                     // to avoid a hot loop, and to allow our node script to
   });                                     // process asynchronous requests in the meantime.
                                           // If you're also serving http, display a 503 error.
-  connection.on('error', function(err) {
+  conn.on('error', function(err) {
     console.log('db error', err);
     if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
       handleDisconnect();                         // lost due to either server restart, or a
